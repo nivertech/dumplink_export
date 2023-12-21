6bc76886ad711be2b414b53597c08f1791031075
Zvi Avraham <zvi@nivertech.com>
2023-12-21 16:29:42 +0200

    initial import

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # dumplink_export
+
+```bash
+curl https://dump.link/api/v1/projects/PROJECTID?token=TOKEN > prroject.json
+node export.js project.json > issue.md
+```
+
+
+GitHub tasklist format (current):
+
+```markdown
+# Project: [test](https://dump.link/a/PROJECTID)
+
+Appetite: 2 weeks
+Updated at: 2023-12-18T13:58:22Z
+
+
+### S1 
+- [x] t11
+- [x] t12
+- [ ] t13
+
+### S2  - Flagged
+- [x] t21
+- [ ] t22
+- [ ] t23
+
+### S3  - Figured out
+- [x] t31
+- [x] t32
+
+### S4  - Done
+- [x] t41
+
+```
+
+GitHub tasklists beta format (new)
+
+```markdown
+# Project: [test](https://dump.link/a/PROJECTID)
+
+Appetite: 2 weeks
+Updated at: 2023-12-18T13:58:22Z
+
+
+```[tasklist]
+### S1 
+- [x] t11
+- [x] t12
+- [ ] t13
+
+```
+
+```[tasklist]
+### S2  - Flagged
+- [x] t21
+- [ ] t22
+- [ ] t23
+
+```
+
+```[tasklist]
+### S3  - Figured out
+- [x] t31
+- [x] t32
+
+```
+
+```[tasklist]
+### S4  - Done
+- [x] t41
+
+```
+
+```
---
+++ export.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+
+const isNewTasklistFormat = false;
+
+// Check if a filename is provided as a command line argument
+const filePath = process.argv[2];
+if (!filePath) {
+    console.error('Please provide the JSON file path as a command line argument.');
+    process.exit(1);
+}
+
+// Read input JSON from file
+const jsonString = fs.readFileSync(filePath, 'utf8');
+const json = JSON.parse(jsonString);
+
+function generateBucketList(json) {
+    const projectId = json.project.id;
+    const buckets = json.buckets || [];
+    const sortedBuckets =
+        buckets
+            .filter(bucket => (bucket.projectId === projectId) && bucket.name !== "")
+            .sort((a, b) => a.priority - b.priority);
+
+    return sortedBuckets;
+}
+
+function generateTaskList(json, bucket) {
+    const bucketId = bucket.id;
+    const tasks = json.tasks || [];
+    const sortedTasks =
+        tasks
+            .filter(task => task.bucketId === bucketId)
+            .sort((a, b) => a.priority - b.priority);
+
+    // if (sortedTasks.length === 0) {
+    //     return null; // Skip empty buckets
+    // }
+
+    const taskList = sortedTasks.map(task => {
+        const checkbox = task.closed ? '[x]' : '[ ]';
+        return `- ${checkbox} ${task.title}`;
+    });
+
+    const isFiguredOut = !bucket.done && sortedTasks.every(task => task.closed);
+    if (bucket.flagged) {
+        bucketState = 'Flagged';
+    } else if (bucket.done) {
+        bucketState = 'Done';
+    } else if (isFiguredOut) {
+        bucketState = 'Figured out';
+    } else {
+        bucketState = '';
+    }
+
+    const allTasks = taskList.join('\n');
+
+    return `### ${bucket.name} ${bucketState !== '' ? ' - ' : ''}${bucketState}\n${allTasks}\n`;
+}
+
+function generateNewTaskList(json, bucket) {
+    return `\`\`\`[tasklist]\n${generateTaskList(json, bucket)}\n\`\`\``;
+}
+
+function generateMarkdown(json) {
+    const buckets = generateBucketList(json);
+
+    const f = isNewTasklistFormat ? generateNewTaskList : generateTaskList;
+
+    //var markdown = buckets.map(bucket => generateTaskList(json, bucket));//.filter(Boolean);
+    var markdown = buckets.map(bucket => f(json, bucket));//.filter(Boolean);
+
+    const project = json.project;
+    const projectURL = `https://dump.link/a/${project.id}`;
+    const projectInfo = `
+# Project: [${project.name}](${projectURL})
+
+Appetite: ${project.appetite} weeks
+Updated at: ${project.updatedAt}\n
+`;
+
+    markdown.unshift(projectInfo);
+    return markdown.join('\n');
+}
+
+const githubMarkdown = generateMarkdown(json);
+console.log(githubMarkdown);
